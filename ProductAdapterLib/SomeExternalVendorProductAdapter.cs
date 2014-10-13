0e0439deb53050490f71ac875a6ba40ab4d7a637
yhLars <lars.ahrlin@yh.nackademin.se>
2014-10-13 13:31:14 +0200

    Added adapter

---
+++ ProductAdapterLib/SomeExternalVendorProductAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductLib;
+
+namespace ProductAdapterLib
+{
+    public class SomeExternalVendorProductAdapter : IProduct
+    {
+        private decimal _unitPrice;
+        private string _barCode;
+        private string _title;
+
+        public decimal UnitPrice
+        {
+            get { return _unitPrice; }
+            set { _unitPrice = value; }
+        }
+
+        public string BarCode
+        {
+            get { return _barCode; }
+            set { _barCode = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+    }
+}
