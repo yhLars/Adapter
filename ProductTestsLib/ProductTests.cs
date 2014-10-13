9178e9e0e0c9713564c253d563e7133fd687464e
yhLars <lars.ahrlin@yh.nackademin.se>
2014-10-13 13:17:53 +0200

    Created basic structure

---
+++ ProductTestsLib/ProductTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProductTestsLib
+{
+    [TestClass]
+    public class ProductTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
