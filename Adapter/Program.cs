7b269c689613a02e21d1cf84c9204f7f222e1ee2
yhLars <lars.ahrlin@yh.nackademin.se>
2014-10-13 14:04:06 +0200

    Lagt till en kommentar i Program.cs för att kunna testa git-credential-winstore.exe application

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             // Skriv kod för att hantera produkter
+
+            // Lagt till en kommentar för att kunna testa git-credential-winstore.exe application
         }
     }
 }
